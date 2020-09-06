8a4c65cc441a605b32432cf5d2a5df2f51db2d74
Vivian Lim <vivlim@pm.me>
2020-09-06 15:38:41 -0700

    add docker-compose and tweak start arguments

--- srb2kart.sh
+++ srb2kart.sh
@@ -5,9 +5,9 @@ cd /usr/games/SRB2Kart || exit
 ADDONS=$(ls /addons)
 
 if [ -z "$ADDONS" ]; then
-    /usr/games/SRB2Kart/srb2kart -dedicated -config kartserv.cfg -home /data $*
+    /usr/games/SRB2Kart/srb2kart -dedicated -config kartserv.cfg -home /data -map map36 -file bonuschars.kart$*
     exit
 fi
 
 # Intentional word splitting
-/usr/games/SRB2Kart/srb2kart -dedicated -config kartserv.cfg -home /data $* -file $ADDONS
+/usr/games/SRB2Kart/srb2kart -dedicated -config kartserv.cfg -home /data $* -file $ADDONS -file bonuschars.kart -map map36
